8d8b60f4d99036180c9aefade76e269bad983a1c
Francisco Moreno <fmoreno@bamboohealth.com>
2023-02-25 16:18:47 -0500

    sd5
    sd5

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -30,7 +30,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Michael Isvy Simple test to make sure that Bean Validation is working (useful
- * when upgrading to a new version of Hibernate Validator/ Bean Validation)
+ * when upgrading to a newer version of Hibernate Validator/ Bean Validation)
  */
 class ValidatorTests {
 
